1679fd2cc83e5c86fbc5f50bac3a365fe0fbe1fb
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2024-04-05 18:17:42 +0200

    [oneDPL][ranges] + tests for ranges::copy, ranges::copy_if, ranges::merge

--- test/parallel_api/ranges/std_ranges.pass.cpp
+++ test/parallel_api/ranges/std_ranges.pass.cpp
@@ -41,9 +41,8 @@ main()
 
     test_range_algo{}(oneapi::dpl::ranges::adjacent_find,  std::ranges::adjacent_find, pred_2, proj);
 
-    test_range_algo<data_in_in>{}(oneapi::dpl::ranges::search,  std::ranges::search, pred_2, proj);
-    test_range_algo<data_in_val_n>{}(oneapi::dpl::ranges::search_n,  std::ranges::search_n, pred_2, proj);
-
+    test_range_algo<data_in_in>{}(oneapi::dpl::ranges::search,  std::ranges::search, pred_2, proj, proj);
+    test_range_algo{}(oneapi::dpl::ranges::search_n,  std::ranges::search_n, 3, 5, pred_2, proj);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
