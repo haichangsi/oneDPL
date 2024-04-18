a5f80e13b6a37508a8bbc87538683feea0b7bff8
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-04-17 22:48:42 -0400

    fix for unnamed lambda
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/experimental/kt/internal/esimd_radix_sort_dispatchers.h
+++ include/oneapi/dpl/experimental/kt/internal/esimd_radix_sort_dispatchers.h
@@ -216,9 +216,15 @@ __onesweep_impl(sycl::queue __q, _RngPack1&& __input_pack, _RngPack2&& __virt_pa
         __esimd_radix_sort_onesweep_histogram<_KernelName>>;
     using _EsimdRadixSortScan = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_provider<
         __esimd_radix_sort_onesweep_scan<_KernelName>>;
-    using _EsimdRadixSortSweep =
-        oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_provider<::std::conditional_t<
-            __has_values, __esimd_radix_sort_onesweep_by_key<_KernelName>, __esimd_radix_sort_onesweep<_KernelName>>>;
+    using _EsimdRadixSortSweepInitial = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_provider<
+        ::std::conditional_t<__has_values, __esimd_radix_sort_onesweep_by_key<_RngPack1, _RngPack2, _KernelName>,
+                             __esimd_radix_sort_onesweep<_RngPack1, _RngPack2, _KernelName>>>;
+    using _EsimdRadixSortSweepEven = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_provider<
+        ::std::conditional_t<__has_values, __esimd_radix_sort_onesweep_by_key<_RngPack2, _RngPack3, _KernelName>,
+                             __esimd_radix_sort_onesweep<_RngPack2, _RngPack3, _KernelName>>>;
+    using _EsimdRadixSortSweepOdd = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_provider<
+        ::std::conditional_t<__has_values, __esimd_radix_sort_onesweep_by_key<_RngPack3, _RngPack2, _KernelName>,
+                             __esimd_radix_sort_onesweep<_RngPack3, _RngPack2, _KernelName>>>;
     using _EsimdRadixSortCopyback = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_provider<
         ::std::conditional_t<__has_values, __esimd_radix_sort_onesweep_copyback_by_key<_KernelName>,
                              __esimd_radix_sort_onesweep_copyback<_KernelName>>>;
@@ -250,7 +256,7 @@ __onesweep_impl(sycl::queue __q, _RngPack1&& __input_pack, _RngPack2&& __virt_pa
         __q, __mem_holder.__global_hist_ptr(), __n, __event_chain);
 
     __event_chain = __radix_sort_onesweep_submitter<__is_ascending, __radix_bits, __data_per_work_item,
-                                                    __work_group_size, _EsimdRadixSortSweep>()(
+                                                    __work_group_size, _EsimdRadixSortSweepInitial>()(
         __q, __input_pack, __virt_pack1, __mem_holder.__global_hist_ptr(), __mem_holder.__group_hist_ptr(),
         __sweep_work_group_count, __n, 0, __event_chain);
 
@@ -263,14 +269,14 @@ __onesweep_impl(sycl::queue __q, _RngPack1&& __input_pack, _RngPack2&& __virt_pa
         if (__stage % 2 != 0)
         {
             __event_chain = __radix_sort_onesweep_submitter<__is_ascending, __radix_bits, __data_per_work_item,
-                                                            __work_group_size, _EsimdRadixSortSweep>()(
+                                                            __work_group_size, _EsimdRadixSortSweepOdd>()(
                 __q, __virt_pack1, __virt_pack2, __p_global_hist, __p_group_hists, __sweep_work_group_count, __n,
                 __stage, __event_chain);
         }
         else
         {
             __event_chain = __radix_sort_onesweep_submitter<__is_ascending, __radix_bits, __data_per_work_item,
-                                                            __work_group_size, _EsimdRadixSortSweep>()(
+                                                            __work_group_size, _EsimdRadixSortSweepEven>()(
                 __q, __virt_pack2, __virt_pack1, __p_global_hist, __p_group_hists, __sweep_work_group_count, __n,
                 __stage, __event_chain);
         }
